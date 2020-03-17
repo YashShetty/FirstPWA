3d4d16fcd262f34831909d2803056d1f098f5a4c
Yash Shetty <yashetty388@gmail.com>
2020-03-17 23:48:18 +1100

    Changed relative paths

--- service-worker.js
+++ service-worker.js
@@ -23,28 +23,28 @@ const DATA_CACHE_NAME = "data-cache-v1";
 
 // CODELAB: Add list of files to cache here.
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/scripts/app.js",
-  "/scripts/install.js",
-  "/scripts/luxon-1.11.4.js",
-  "/styles/inline.css",
-  "/images/add.svg",
-  "/images/clear-day.svg",
-  "/images/clear-night.svg",
-  "/images/cloudy.svg",
-  "/images/fog.svg",
-  "/images/hail.svg",
-  "/images/install.svg",
-  "/images/partly-cloudy-day.svg",
-  "/images/partly-cloudy-night.svg",
-  "/images/rain.svg",
-  "/images/refresh.svg",
-  "/images/sleet.svg",
-  "/images/snow.svg",
-  "/images/thunderstorm.svg",
-  "/images/tornado.svg",
-  "/images/wind.svg"
+  "/MIDIScript",
+  "/MIDIScript/index.html",
+  "/MIDIScript/scripts/app.js",
+  "/MIDIScript/scripts/install.js",
+  "/MIDIScript/scripts/luxon-1.11.4.js",
+  "/MIDIScript/styles/inline.css",
+  "/MIDIScript/images/add.svg",
+  "/MIDIScript/images/clear-day.svg",
+  "/MIDIScript/images/clear-night.svg",
+  "/MIDIScript/images/cloudy.svg",
+  "/MIDIScript/images/fog.svg",
+  "/MIDIScript/images/hail.svg",
+  "/MIDIScript/images/install.svg",
+  "/MIDIScript/images/partly-cloudy-day.svg",
+  "/MIDIScript/images/partly-cloudy-night.svg",
+  "/MIDIScript/images/rain.svg",
+  "/MIDIScript/images/refresh.svg",
+  "/MIDIScript/images/sleet.svg",
+  "/MIDIScript/images/snow.svg",
+  "/MIDIScript/images/thunderstorm.svg",
+  "/MIDIScript/images/tornado.svg",
+  "/MIDIScript/images/wind.svg"
 ];
 
 self.addEventListener("install", evt => {
@@ -82,7 +82,7 @@ self.addEventListener("activate", evt => {
 self.addEventListener("fetch", evt => {
   console.log("[ServiceWorker] Fetch", evt.request.url);
   // CODELAB: Add fetch event handler here.
-  if (evt.request.url.includes("/forecast/")) {
+  if (evt.request.url.includes("/MIDIScript/forecast/")) {
     console.log("[Service Worker] Fetch (data)", evt.request.url);
     evt.respondWith(
       caches.open(DATA_CACHE_NAME).then(cache => {
