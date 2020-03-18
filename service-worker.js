8693f0be12dc3e8811523973c19816a012c8ea88
Yash Shetty <yashetty388@gmail.com>
2020-03-18 19:07:46 +1100

    Fixed error handling

--- service-worker.js
+++ service-worker.js
@@ -54,8 +54,8 @@ self.addEventListener("install", evt => {
     caches.open(CACHE_NAME).then(cache => {
       console.log("[ServiceWorker] Pre-caching offline page");
       return cache.addAll(FILES_TO_CACHE);
-    }).catch(e => {
-      console.log(e);
+    },e => {
+      console.log("error:" + e);
     });
   );
 
