086bbdf9c62574cf8a8e7836bebbd5740de5f55e
Yash Shetty <yashetty388@gmail.com>
2020-03-18 18:52:13 +1100

    install error catching

--- service-worker.js
+++ service-worker.js
@@ -54,7 +54,9 @@ self.addEventListener("install", evt => {
     caches.open(CACHE_NAME).then(cache => {
       console.log("[ServiceWorker] Pre-caching offline page");
       return cache.addAll(FILES_TO_CACHE);
-    })
+    }).catch(e => {
+      console.log(e);
+    });
   );
 
   self.skipWaiting();
